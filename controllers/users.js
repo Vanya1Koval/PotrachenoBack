a67213e4176d15140a555f37649a61ce83b9c784
Vanya1Koval <103115000+Vanya1Koval@users.noreply.github.com>
2022-04-14 20:11:59 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,57 +1,58 @@
 const userServiceClass = require('../services/users')
-const userService = new userServiceClass() 
+const userService = new userServiceClass()
 
 
-const  getAllUsers = async (req, res) =>  {
+const getAllUsers = async (req, res) => {
     const { id } = req.body;
-    if (id == undefined) {
+    if (!id) {
         const users = await userService.getAll();
         res.json(users);
     }
     else {
-        const user =  await userService.getOne(id);
-        if (user[0] == undefined) {
-            res.status(404).send(`User with id ${id} doesn't exist`)
-        } else {
+        const user = await userService.getOne(id);
+        if (user[0]) {
             res.json(user);
-        } 
+        } else {
+            res.status(404).send(`User with id ${id} doesn't exist`)
+        }
     }
-};  
+};
 
-const createUser = async (req, res) =>  {
+const createUser = async (req, res) => {
     const { id, name } = req.body;
     const oneUser = await userService.getOne(id);
-    if (oneUser[0] == undefined) { 
+    if (oneUser[0]) {
+        res.status(404).send(`User with id ${id} already exist`);
+    }
+    else {
         const user = await userService.create(id, name);
         res.json(user);
     }
-    else {
-        res.status(404).send(`User with id ${id} already exist`);
-    }
 };
 
-const updateUser = async (req, res) =>  {
+const updateUser = async (req, res) => {
     const { id, name } = req.body;
     const user = await userService.getOne(id);
-    if (user[0] == undefined) {
-        res.status(404).send(`User with id ${id} doesn't exist`)
-    }
-    else {
+    if (user[0]) {
         const updatedUser = await userService.update(id, name);
         res.json(updatedUser);
     }
-};
-
-const deleteUser = async (req, res) =>  {
-    const { id } = req.body;
-    const user = await userService.getOne(id);
-    if (user[0] == undefined) {
+    else {
         res.status(404).send(`User with id ${id} doesn't exist`)
     }
-    else {
-        const deleted = await userService.delete(id);
+};
+
+const deleteUser = async (req, res) => {
+    const { id } = req.body;
+    const user = await userService.getOne(id);
+    if (user[0]) {
+        await userService.delete(id);
         res.send(`user with id ${id} was deleted`);
+    }
+    else {
+        res.status(404).send(`User with id ${id} doesn't exist`)
     }
 };
 
-module.exports = {  getAllUsers, createUser, updateUser, deleteUser };
+module.exports = { getAllUsers, createUser, updateUser, deleteUser };
+
