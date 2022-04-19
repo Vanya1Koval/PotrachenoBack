71c03ba4ad63525cf22951a9bf9ac4e2c8e4c5cb
Vanya1Koval <103115000+Vanya1Koval@users.noreply.github.com>
2022-04-19 19:23:34 +0300

    Update auth.js

--- middlware/auth.js
+++ middlware/auth.js
@@ -4,7 +4,7 @@ function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
   
-    if (token == null) return res.sendStatus(401)
+    if (token === null) return res.sendStatus(401)
   
     jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
       console.log(err)
