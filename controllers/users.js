f250de6d768ca8d867247d6f510d3a30a25580fe
Vanya1Koval <103115000+Vanya1Koval@users.noreply.github.com>
2022-04-20 17:34:44 +0300

    Create users.js

---
+++ controllers/users.js
@@ -0,0 +1,52 @@
+const userServiceClass = require('../services/users')
+const bcrypt = require('bcrypt');
+const multer = require('../middlware/multer.js');
+const userService = new userServiceClass()
+
+const salt = bcrypt.genSaltSync(10);
+
+const getAllUsers = async (req, res) => {
+    const { _id } = req.body;
+    try {
+        if (!_id) {
+            const users = await userService.getAll();
+            return res.json(users);
+        }
+        else {
+            const user = await userService.getOne(_id);
+            return res.json(user); 
+        }
+    } catch(e) {
+        console.log('Error ' + e.name + ":" + e.message + "\n" + e.stack);
+        return res.status(404).send(`User with id ${_id} doesn't exist`);
+    }
+};
+
+const updateUser = async (req, res) => {
+    try {
+        const { _id, name, login, password  } = req.body;
+        const passwordHash = bcrypt.hashSync(password, salt)
+        const updatedUser = await userService.update(_id, name, login, passwordHash);
+        return res.json(updatedUser);
+    } catch(e) {
+        console.log('Error ' + e.name + ":" + e.message + "\n" + e.stack);
+    }
+};
+
+const deleteUser = async (req, res) => {
+    const { _id } = req.body;
+    try{
+        if(_id){
+            await userService.delete(_id);      
+            res.send(`User with id ${_id} was deleted`);
+        }
+        else{
+            return res.status(404).send(`User with id ${_id} doesn't exist`);
+        }
+    } catch(e) {
+        console.log('Error ' + e.name + ":" + e.message + "\n" + e.stack); 
+        return res.status(404).send(`User with id ${_id} doesn't exist`);
+    }  
+};
+
+module.exports = { getAllUsers, updateUser, deleteUser };
