80a80268bea75ba9bc8475150b62a6fb1c9b1611
Vanya1Koval <103115000+Vanya1Koval@users.noreply.github.com>
2022-06-19 16:01:15 +0300

    Update friends.js

--- routes/friends.js
+++ routes/friends.js
@@ -3,8 +3,5 @@ const router = express.Router();
 const friends = require('../controllers/user/friends')
 
 router.post('/', friends.addRequest);
-//router.put('/', friends.updateRequests);
-//router.delete('/', friends.updateRequests);
-
 
 module.exports = router;
